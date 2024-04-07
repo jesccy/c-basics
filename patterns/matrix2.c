9ee6d2acc63dc1955923203e2f3ed8a272db4b0d
kim seojan <bisha5546657@gmail.com>
2024-04-07 15:36:04 +0530

    matrixpattern

---
+++ patterns/matrix2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int i = 0, j=0;
+
+    for (i = 0; i <= 1; i++)
+    {
+        for (j = 0; j <= 1; j++)
+        {
+            printf("@");
+        }
+        printf("\n");
+    }
+    return 0;
+}
