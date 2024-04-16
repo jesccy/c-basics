6551fd27e8295e08ae63041b12691b6503ae37bb
Jass Preet <jaspreet3480@gmail.com>
2024-04-16 12:22:53 +0530

    hollow square

--- patterns/hollow-square.c
+++ patterns/hollow-square.c
@@ -4,37 +4,26 @@ int main()
     int i = 0;
     int j;
     int k;
-    int num;
-    int half;
+    int num = 5;
+
     printf("\n Please Enter odd number of rows to print diamond:");
     scanf("\n%d", &num);
-    half = (num-1)/2;
-    for (i = 0; i <= half; i++)
+
+    for (i = 0; i < num; i++)
     {
-        for (j = 0; j < half - i; j++)
+        for (j = 0; j < num; j++)
         {
-            printf("  ");
+
+            if (i!=0 && j >= 1 && j < num-1 && i!= num-1)
+            {
+                printf("  ");
+            }
+            else
+            {
+                printf("* ");
+            }
         }
-        for (j = 0; j < (2 * i) + 1; j++)
-        {
-            printf("* ");
-        }
-        printf("\n");
-    }
-    for (i = half+1; i <= (num-1); i++)
-    {
-        for (j = 0; j < (i - (half+1)) + 1; j++)
-        {
-            printf("  ");
-        }
-        for (j = 0; j < (num) - i; j++)
-        {
-            printf("* ");
-        }
-        for (j = 0; j < (num-1) - i; j++)
-        {
-            printf("* ");
-        }
+
         printf("\n");
     }
 }
