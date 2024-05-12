2e8f7236ca426031dad54cf79b1d68ff2536ec59
kim seojan <bisha5546657@gmail.com>
2024-05-12 18:49:33 +0530

    pointer

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 int main()
 {
-    int num, remainder;
-    printf("\n Enter a number  to print reverse of number");
-    scanf("\n%d", &num);
-    while (num != 0)
-    {
-        remainder = num % 10;
-        printf("%d", remainder);
-        num/=10;
-    }
-
+    int num, remainder,num1=2;
+int *ptr=&num1;
+printf("%p",&num1);
     return 0;
 }
